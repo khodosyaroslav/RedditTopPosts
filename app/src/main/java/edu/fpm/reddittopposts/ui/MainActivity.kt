23a7a8c098c9a7396c52d403c57a66c0b7e20437
Yaroslav Khodos <khodos.jar@gmail.com>
2023-06-06 15:30:44 +0300

    implemented basic ui

--- app/src/main/java/edu/fpm/reddittopposts/ui/MainActivity.kt
+++ app/src/main/java/edu/fpm/reddittopposts/ui/MainActivity.kt
@@ -3,10 +3,16 @@ package edu.fpm.reddittopposts.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import edu.fpm.reddittopposts.R
+import edu.fpm.reddittopposts.databinding.ActivityMainBinding
+import edu.fpm.reddittopposts.databinding.PostItemBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
